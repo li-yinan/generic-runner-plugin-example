09439e000969d69f497f67460e2f3bb668197fe5
li-yinan <biline@163.com>
2017-08-30 14:20:06 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
-export {Engine} from './engine';
-export {
-    buildFlowFromConfig,
-    addCustomNodeSeachPath,
-    deserialize,
-    getNodes,
-    postfix,
-    serialize
-} from './nodes/util/util';
-import node from './nodes/util/node';
-import returnvalue from './nodes/util/returnvalue';
-
-export const Node = node;
-export const ReturnValue = returnvalue;
-
+console.log('init');
