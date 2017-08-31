b134d5bb29933eb48068df10b56c8ab4b28adab4
li-yinan <biline@163.com>
2017-08-31 21:50:43 +0800

    模板ready

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('init');
+import {
+    Node,
+    ReturnValue
+} from 'genericrunner';
+
+export default class Console extends Node {
+
+    static type = 'example';
+
+    out = 0;
+
+    async exec(param) {
+        super.exec(param);
+        console.log('example');
+        return new ReturnValue(0, null, this);
+    }
+}
